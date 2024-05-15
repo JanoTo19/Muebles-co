56932cdc5b0ed948843f08c920d467ec42803005
Javi <you@example.com>
2024-05-15 08:47:59 +0200

    :3

--- src/graphical/Aplication.java
+++ src/graphical/Aplication.java
@@ -3,10 +3,22 @@ package graphical;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JTable;
+import javax.swing.JComboBox;
+import javax.swing.border.BevelBorder;
+import java.awt.Color;
+import javax.swing.UIManager;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Aplication {
 
 	private JFrame frame;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -35,10 +47,67 @@ public class Aplication {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		String tiempo="";
 		frame = new JFrame();
-		frame.setBounds(100, 100, 643, 492);
+		frame.setBounds(100, 100, 730, 548);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-	}
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(277, 11, 46, 14);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("Salir");
+		btnNewButton.setBounds(56, 39, 123, 45);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnAadir = new JButton("Añadir");
+		btnAadir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnAadir.setToolTipText("Añadir");
+		btnAadir.setBounds(235, 39, 123, 45);
+		frame.getContentPane().add(btnAadir);
+		
+		JButton btnMostrarProductos = new JButton("Mostrar productos");
+		btnMostrarProductos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnMostrarProductos.setBounds(431, 39, 228, 45);
+		frame.getContentPane().add(btnMostrarProductos);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, ratata(), null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+			},
+			new String[] {
+				"New column", "New column", "New column", "New column"
+			}
+		));
+		table.setBounds(66, 147, 550, 273);
+		frame.getContentPane().add(table);
+		
 
+	 
+	        
+	}public String ratata() {
+		return "b";
+	}
 }
